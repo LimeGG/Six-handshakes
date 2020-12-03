9f24750f616ec750ec1ff297637ec44ba19b141e
Krioks <core.safecrlf>
2020-12-03 20:08:22 +0300

    add function logon

--- first_req.py
+++ first_req.py
@@ -2,6 +2,7 @@ import requests
 from selenium import webdriver
 import os
 import time
+import json
 
 
 headers = {
@@ -27,8 +28,32 @@ dc = {'Данила Козловский': 'https://www.kinopoisk.ru/name/105495
 
 driver = webdriver.Firefox()
 driver.get("https://www.kinopoisk.ru")
+# driver.get("https://www.kinopoisk.ru/name/1054956/")
 time.sleep(1)
-driver.get("https://www.kinopoisk.ru/name/1054956/")
+submit_api = driver.find_element_by_xpath('/html/body/div[1]/div/div[5]/header/div/div/div[3]/div/button')
+submit_api.click()
+time.sleep(1)
+with open('creditans.json') as f:
+    cred = json.load(f)
+
+login = driver.find_element_by_xpath('//*[@id="passp-field-login"]')
+login.clear()
+login.send_keys(cred['username'])
+
+element_login = driver.find_element_by_xpath('/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[3]')
+element_login.click()
+
+login_pass = driver.find_element_by_xpath('//*[@id="passp-field-passwd"]')
+login_pass.send_keys(cred['password'])
+
+element_login = driver.find_element_by_xpath('/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/form/div[3]/button')
+element_login.click()
+
+try:
+    phone_number = driver.find_element_by_xpath('/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/form/div[3]/button')
+    phone_number.click()
+except:
+    print()
 
 
 time.sleep(25)
