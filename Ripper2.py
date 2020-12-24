82d4a015ef314dddc1a4b6ab535597bc37d398b9
Krioks <core.safecrlf>
2020-12-24 21:21:03 +0300

    Edit Ripper2.py

--- Ripper2.py
+++ Ripper2.py
@@ -1,45 +1,45 @@
+import os
+import json
 from bs4 import BeautifulSoup
-with open('Word.html', 'r', encoding='utf-8') as f:
-    s = f.read()
-def cinema(s):
-    soup = BeautifulSoup(s, features="lxml")
+
+
+def cinema(film):
+    soup = BeautifulSoup(film, features="lxml")
+    raw_film_name = soup.find_all('h1')[1].find('a')
+    film_href = f"https://www.kinopoisk.ru{raw_film_name.get('href')}"
+    raw_film_name_str = str(raw_film_name)
+    film_name = raw_film_name_str[raw_film_name_str.find('>')+1:raw_film_name_str.find('</a')]
     raw_list = soup.find_all('div', class_="name")
 
-    clear_dict = {}
+
+    clear_list = []
     for i in range(len(raw_list)):
         temp = str(raw_list[i])
-        temp_clear = temp[temp.find('f=')+3:temp.find('</a')].split('">')
-        clear_dict[temp_clear[1]] = "https://www.kinopoisk.ru/" + temp_clear[0]
+        temp_clear = temp[temp.find('f=') + 3:temp.find('</a')].split('">')
+        clear_list.append(dict(actor_name=temp_clear[1], actor_href="https://www.kinopoisk.ru/" + temp_clear[0]))
 
-    return clear_dict
-with open('Word.html', 'r', encoding='utf-8') as f:
-    s = f.read()
-with open('strel.html', 'r', encoding='utf-8') as f:
-    g = f.read()
-with open('ice.html', 'r', encoding='utf-8') as f:
-    t = f.read()
-with open('Gogol.html', 'r', encoding='utf-8') as f:
-    i = f.read()
-with open('Hero.html', 'r', encoding='utf-8') as f:
-    y = f.read()
-with open('Method.html', 'r', encoding='utf-8') as f:
-    o = f.read()
-with open('sad.html', 'r', encoding='utf-8') as f:
-    p = f.read()
-with open('vtor.html', 'r', encoding='utf-8') as f:
-    n = f.read()
-with open('Anna.html', 'r', encoding='utf-8') as f:
-    z = f.read()
-with open('born.html', 'r', encoding='utf-8') as f:
-    x = f.read()
+    return dict(film_name=film_name, film_href=film_href, actors=clear_list)
 
-print(cinema(x))
-print(cinema(z))
-print(cinema(n))
-print(cinema(p))
-print(cinema(o))
-print(cinema(y))
-print(cinema(i))
-print(cinema(t))
-print(cinema(s))
-print(cinema(g))
+
+def ripper(dir_with_films):
+    for film in dir_with_films:
+        with open(f'Films\\{film}', 'r', encoding='utf-8') as f:
+            ds = cinema(f.read())
+            with open(f'Clear_films\\{ds["film_name"]}.json', 'w') as g:
+                json.dump(ds, g, indent=2)
+
+
+
+
+
+
+
+def main():
+    dir_with_films = os.listdir('Films')
+    ripper(dir_with_films)
+
+
+if __name__ == '__main__':
+    main()
+
+
