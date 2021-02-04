424ebd850c18c5b03dc81f8db05d543a9b97907d
Krioks <core.safecrlf>
2021-02-04 20:04:33 +0300

    Add graph method

--- json_ripper.py
+++ json_ripper.py
@@ -42,14 +42,42 @@ actors = [{
     ]
 }]
 
+# graph = {}
+# temp = False
+# for i in range(len(actors)):
+#     # actors[i]
+#     graph[actors[i]['actor_href']] = []
+#     for j in range(i+1, len(actors)):
+#         for film in actors[i]['films']:
+#             if film in actors[j]['films']:
+#                 temp = film in actors[j]['films']
+#                 # print(actors[j]['actor_name'])
+#
+# print(graph)
+graph = {}
 temp = False
 for i in range(len(actors)):
-    # actors[i]
-    for j in range(i+1, len(actors)):
-        for film in actors[i]['films']:
-            if film in actors[j]['films']:
-                temp = film in actors[j]['films']
-                print(actors[j]['actor_name'])
+    graph[actors[i]['actor_name']] = []
+    for j in range(len(actors)):
+        if actors[j]['actor_name'] != actors[i]['actor_name']:
+            for film in actors[i]['films']:
+                if film in actors[j]['films']:
+                    temp = film in actors[j]['films']
+                    graph[actors[i]['actor_name']].append(actors[j]['actor_name'])
 
+#
+print(graph)
 
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]  # (vertex, path)
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in set(graph[vertex]) - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
 
+# print(list(dfs_paths(graph, 'A', 'F')))
+
+print(list(dfs_paths(graph, actors[1]['actor_name'], actors[2]['actor_name'])))
