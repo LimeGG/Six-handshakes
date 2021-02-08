034df13b2222a1f8597d3e0f03f71731c485174f
Krioks <core.safecrlf>
2021-02-08 21:02:47 +0300

    Edit ripper Add General_Ripper.py

--- Get_actors.py
+++ Get_actors.py
@@ -27,7 +27,7 @@ def login(driver):
     time.sleep(1)
     with open('creditans.json') as f:
         cred = json.load(f)
-
+    time.sleep(1)
     login = driver.find_element_by_xpath('//*[@id="passp-field-login"]')
     login.clear()
     login.send_keys(cred['username'])
@@ -50,8 +50,17 @@ def login(driver):
     except Exception as e:
         print(e)
 
+
 def get_actor_page(driver, actor_href):
     driver.get(actor_href)
+    # '/html/body/div[1]/div/div[2]/div[4]/div/div/div[1]/div/div/div[7]/div/div[1]/nav/div/div[1]/div/button[1]/span[2]'
+    # '/html/body/div[1]/div/div[2]/div[4]/div/div/div[1]/div/div/div[7]/div/div[3]/div/div[79]/div/span'
+    # WebElement
+    # element = driver.findElement(By.id("id_of_element"));
+    # ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    # Thread.sleep(500);
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
     return driver.page_source
 
 
@@ -61,7 +70,7 @@ for key in dict_with_actors.keys():
     time.sleep(2)
     with open(f'Actors\\{key}.html', 'w', encoding='utf-8') as f:
         f.write(get_actor_page(driver_firefox, dict_with_actors[key]))
-
+        print(f'{key} was written!')
 
 time.sleep(25)
 
--- ripper_sites.py
+++ ripper_sites.py
@@ -1,28 +1,30 @@
 from bs4 import BeautifulSoup
+import os
+from pathlib import PureWindowsPath
 
+for file in os.listdir('Actors'):
+    with open(PureWindowsPath('Actors', file), 'r', encoding='utf-8') as f:
+        s = f.read()
 
-with open('Александр Петров — фильмы — КиноПоиск.htm', 'r', encoding='utf-8') as f:
-    s = f.read()
+    soup = BeautifulSoup(s, features="lxml")
 
-soup = BeautifulSoup(s, features="lxml")
+    raw_list = soup.find_all('a')
+    clear_ = []
 
-raw_list = soup.find_all('a')
-clear_ = []
+    for i in range(len(raw_list)):
+        if str(raw_list[i]).count('styles_mainTitle'):
+            clear_.append(raw_list[i])
 
-for i in range(len(raw_list)):
-    if str(raw_list[i]).count('styles_mainTitle'):
-        clear_.append(raw_list[i])
+    temp = str(clear_[0])
 
-temp = str(clear_[0])
+    clear_dict = {}
+    for i in range(len(clear_)):
+        temp = str(clear_[i])
+        temp_clear = temp[temp.find('span class="styles_mainTitle') + 1:temp.find('</span>')]
+        clear = temp_clear[temp_clear.find('>') + 1:]
+        clear_dict[clear] = clear_[i].get('href')
 
-clear_dict = {}
-for i in range(len(clear_)):
-    temp = str(clear_[i])
-    temp_clear = temp[temp.find('span class="styles_mainTitle') + 1:temp.find('</span>')]
-    clear = temp_clear[temp_clear.find('>') + 1:]
-    clear_dict[clear] = clear_[i].get('href')
-
-print(clear_dict)
+    print(clear_dict)
 
 
 
